ccab69badf9de555ef97ab9776dc124d7ae07692
jfchilito7 <jfchilito7@gmail.com>
2024-07-09 22:54:50 -0500

    chore: Update webpack.config.js to use CleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const loader = require('sass-loader');
 const TersePlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
